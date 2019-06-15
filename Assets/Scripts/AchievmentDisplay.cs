c52796c70f4aa13a2bc9decca4c0fd409e2c0de0
Clauber Rogério de Oliveira Câmara <clauberogerioo@gmail.com>
2019-06-15 15:40:52 -0300

    reorganizacao de arquivos

---
+++ Assets/Scripts/AchievmentDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievmentDisplay : MonoBehaviour
+{
+
+    public Achievment Achievment;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+}
