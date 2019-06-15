bcb670a2f63690f441141a7987a85c7414e62d8c
Will <will.lucen4@gmail.com>
2019-06-15 14:56:32 -0300

    MenuController created, working on player script

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenuGO;
+    [SerializeField] private GameObject settingsMenuGO;
+    [SerializeField] private GameObject aboutMenuGO;
+
+    private GameObject currentMenu;
+
+    public void goToMain()
+    {
+        currentMenu.SetActive(false);
+        currentMenu = mainMenuGO;
+        currentMenu.SetActive(true);
+    }
+
+    public void goToSettings()
+    {
+        currentMenu.SetActive(false);
+        currentMenu = settingsMenuGO;
+        currentMenu.SetActive(true);
+    }
+
+    public void goToAbout()
+    {
+        currentMenu.SetActive(false);
+        currentMenu = aboutMenuGO;
+        currentMenu.SetActive(true);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void goToGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+}
