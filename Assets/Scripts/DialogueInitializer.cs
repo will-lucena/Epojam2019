0ea3173010f94839c25ac51c25e1a7923b1e3921
Clauber Rogério de Oliveira Câmara <clauberogerioo@gmail.com>
2019-06-16 09:59:31 -0300

    animação do dos dialogos

---
+++ Assets/Scripts/DialogueInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueInitializer : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void InitializerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
